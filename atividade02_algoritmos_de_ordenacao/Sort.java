52df4250c1bcd4e8a43f1d033ded7219e064b094
Letícia Cruz <dacruzzzleticia@gmail.com>
2025-09-12 08:01:38 -0300

    fix: for do inserction sort - atividade #02

--- atividade02_algoritmos_de_ordenacao/Sort.java
+++ atividade02_algoritmos_de_ordenacao/Sort.java
@@ -51,9 +51,11 @@ public class Sort {
     public static void inserction(int[] vetorA, int n) {
 
         for (int i = 1; i < n; i++) {
+            
             int key = vetorA[i];
             int j = i - 1;
-            while (j > 0 && vetorA[j] > key) {
+            
+            while (j >= 0 && vetorA[j] > key) {
                 vetorA[j + 1] = vetorA[j];
                 j--;
             }
