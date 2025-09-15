90e61869f2f4bd4324eed5c253ed6e3eee69ceb8
Letícia Cruz <dacruzzzleticia@gmail.com>
2025-09-15 09:24:18 -0300

    fix: for interno bubble sort - atividade #02

--- atividade02_algoritmos_de_ordenacao/Sort.java
+++ atividade02_algoritmos_de_ordenacao/Sort.java
@@ -5,7 +5,7 @@ public class Sort {
     public static void bubbleClassico(int[] vetorA, int n) {
 
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1; j++) {
+            for (int j = 0; j < n - i - 1; j++) {
                 if (vetorA[j] > vetorA[j + 1]) {
                     Utils.troca(vetorA, j, j + 1);
 
